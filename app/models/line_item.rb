94101b791f2ed11d444c6f4c6439c604d1bc4356
mulixiang199016 <mu_lixiang@vanceinfo.com>
2012-07-31 18:19:02 +0800

    smart cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :cart_id, :product_id
+  attr_accessible :product, :cart_id, :product_id, :quantity
  
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
