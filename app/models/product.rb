ebf09da838fd654d92a98a93a7c99626621cfbc2
mulixiang199016 <mu_lixiang@vanceinfo.com>
2012-08-02 18:38:11 +0800

    check out

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   default_scope order: 'title'
   
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_be_any_line_item
   
   attr_accessible :description, :image_url, :price, :title
